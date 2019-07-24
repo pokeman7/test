34926037b8ea7b821e6a6ccd1e199113cb1a057d
pokeman7 <1766446371@qq.com>
2019-07-24 15:13:22 +0800

    界面

---
+++ interface/app/src/main/java/com/example/aninterface/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.aninterface;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_start = findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("usename","苟欢欢");
+                startActivity(intent);
+            }
+        });
+    }
+}
